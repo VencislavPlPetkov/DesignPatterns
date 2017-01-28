6eeac3ca413795ebd806216b3c3559f8539bd505
VencislavPlPetkov <vencislavplpetkov@yahoo.com>
2017-01-28 06:52:09 +0200

    Add Files

---
+++ iteratorPattern/SongsOfThe70s.java
@@ -0,0 +1,36 @@
+package iteratorPattern;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SongsOfThe70s implements SongIteratorInterface {
+
+	ArrayList<SongInfo> bestSongs;
+
+	public SongsOfThe70s() {
+
+		bestSongs = new ArrayList<SongInfo>();
+
+		addSong("Imagine", "John Lennon", 1971);
+		addSong("American Pie", "Don McLean", 1971);
+		addSong("I Will Survive", "Gloria Gaynor", 1979);
+
+	}
+
+	private void addSong(String songName, String bandName, int yearReleased) {
+
+		SongInfo songInfo = new SongInfo(songName, bandName, yearReleased);
+
+		bestSongs.add(songInfo);
+
+	}
+	/*
+	 * public ArrayList<SongInfo> getBestSongs() { return bestSongs; }
+	 */
+
+	public Iterator createIterator() {
+
+		return bestSongs.iterator();
+	}
+
+}
