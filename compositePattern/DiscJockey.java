1dab73a2754ebc937c091524a08dae183ad73617
VencislavPlPetkov <vencislavplpetkov@yahoo.com>
2017-01-30 06:24:48 +0200

    Add Files

---
+++ compositePattern/DiscJockey.java
@@ -0,0 +1,15 @@
+package compositePattern;
+
+public class DiscJockey {
+
+	SongComponent songList;
+
+	public DiscJockey(SongComponent newSongList) {
+		songList = newSongList;
+	}
+
+	public void getSongList() {
+		songList.displaySongInfo();
+	}
+
+}
